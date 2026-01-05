f87bb98504150a1c105b2dd474ef478b8cb9ff10
payusov <drozdovskiy1919@gmail.com>
2026-01-06 00:48:12 +0300

    Clean main branch

--- src/components/shared/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ProductCard } from './ProductCard';
-// Добавлять сюда другие shared-компоненты по мере создания
--- src/components/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export { Button } from './Button';
-export { Input } from './Input';
-// Добавлять сюда другие UI-компоненты по мере создания
